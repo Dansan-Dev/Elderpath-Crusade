db407e4caee897c13b6630bb9ec48cbe1978054d
Daniel Sandstrom <github@danielsandstrom.org>
2025-10-13 14:15:37 +0200

    Implmented navigation from Main Menu to Demo room

--- core/src/main/java/io/github/forest_of_dreams/rooms/MainMenuRoom.java
+++ core/src/main/java/io/github/forest_of_dreams/rooms/MainMenuRoom.java
@@ -2,8 +2,10 @@ package io.github.forest_of_dreams.rooms;
 
 import com.badlogic.gdx.graphics.Color;
 import io.github.forest_of_dreams.data_objects.Box;
+import io.github.forest_of_dreams.data_objects.ClickableEffectData;
 import io.github.forest_of_dreams.data_objects.Text;
 import io.github.forest_of_dreams.enums.FontType;
+import io.github.forest_of_dreams.managers.Game;
 import io.github.forest_of_dreams.managers.SettingsManager;
 import io.github.forest_of_dreams.supers.Room;
 
@@ -18,6 +20,13 @@ public class MainMenuRoom extends Room {
         titleBox.setX(screen_center[0] - titleBox.getWidth()/2);
         titleBox.setY(screen_height - titleBox.getHeight() - 50);
         addContent(title);
+
+        Text playButton = new Text("Play Demo", FontType.DEFAULT, 0, 0, 0, Color.BLUE)
+            .withOnClick((e) -> Game.gotoRoom(StartRoom.get()), ClickableEffectData.getImmediate());
+        Box playButtonBox = playButton.getBounds();
+        playButtonBox.setX(screen_center[0] - playButtonBox.getWidth()/2);
+        playButtonBox.setY(screen_center[1] - playButtonBox.getHeight()/2);
+        addContent(playButton);
     }
 
     public static MainMenuRoom get() {
