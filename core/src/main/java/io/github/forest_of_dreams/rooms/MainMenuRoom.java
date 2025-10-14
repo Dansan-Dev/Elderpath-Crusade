aa1d2cae91c724c01491339dc76775f145e742e7
Daniel Sandstrom <github@danielsandstrom.org>
2025-10-14 10:24:49 +0200

    Add main menu background

--- core/src/main/java/io/github/forest_of_dreams/rooms/MainMenuRoom.java
+++ core/src/main/java/io/github/forest_of_dreams/rooms/MainMenuRoom.java
@@ -1,19 +1,40 @@
 package io.github.forest_of_dreams.rooms;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import io.github.forest_of_dreams.data_objects.Box;
 import io.github.forest_of_dreams.data_objects.ClickableEffectData;
 import io.github.forest_of_dreams.data_objects.Text;
 import io.github.forest_of_dreams.enums.FontType;
+import io.github.forest_of_dreams.enums.SpriteBoxPos;
+import io.github.forest_of_dreams.game_objects.SpriteObject;
 import io.github.forest_of_dreams.managers.Game;
 import io.github.forest_of_dreams.managers.SettingsManager;
 import io.github.forest_of_dreams.supers.Room;
+import io.github.forest_of_dreams.utils.SpriteCreator;
+
+import java.util.List;
 
 public class MainMenuRoom extends Room {
     private MainMenuRoom() {
         super();
         int[] screen_center = SettingsManager.screenSize.getScreenCenter();
+        int screen_width = SettingsManager.screenSize.getScreenWidth();
         int screen_height = SettingsManager.screenSize.getScreenHeight();
+
+        int[] backgroundSize = {1536, 1024};
+        SpriteObject background = new SpriteObject(0, 0, screen_width, screen_height, 1, SpriteBoxPos.CENTER);
+        background.addAnimation(
+            "general",
+            List.of(SpriteCreator.makeSprite(
+                "images/home_screen_background.png",
+                0, 0,
+                backgroundSize[0], backgroundSize[1],
+                screen_width, screen_height
+            )),
+            0
+        );
+        addContent(background);
 
         Text title = new Text("Main Menu", FontType.DEFAULT, 0, 0, 0, Color.WHITE);
         Box titleBox = title.getBounds();
--- core/src/main/java/io/github/forest_of_dreams/utils/SpriteCreator.java
+++ core/src/main/java/io/github/forest_of_dreams/utils/SpriteCreator.java
@@ -4,7 +4,21 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
+/**
+ * Utility class for creating sprites.
+ */
 public class SpriteCreator {
+    /**
+     *
+     * @param path the path to the image (ex. "images/image.png")
+     * @param sheetX the x coordinate of the sprite in the sheet
+     * @param sheetY the y coordinate of the sprite in the sheet
+     * @param width the width of the sprite in the sheet
+     * @param height the height of the sprite in the sheet
+     * @param newWidth the width of the sprite after scaling
+     * @param newHeight the height of the sprite after scaling
+     * @return
+     */
     public static Sprite makeSprite(String path, int sheetX, int sheetY, int width, int height, int newWidth, int newHeight) {
         Texture texture = new Texture(Gdx.files.internal(path));
         Sprite sprite = new Sprite(texture, sheetX, sheetY, width, height);
