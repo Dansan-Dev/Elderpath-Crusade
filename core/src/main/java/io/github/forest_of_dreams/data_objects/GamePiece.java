e1091dc194098db6ee3e50883e8bd8eacd22001f
Daniel Sandstrom <github@danielsandstrom.org>
2025-10-18 15:31:56 +0200

    Clean gamepiece from being a renderable and clickable

--- core/src/main/java/io/github/forest_of_dreams/data_objects/GamePiece.java
+++ core/src/main/java/io/github/forest_of_dreams/data_objects/GamePiece.java
@@ -16,7 +16,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
 
-public abstract class GamePiece extends AbstractTexture implements Renderable, Clickable {
+public abstract class GamePiece {
     @Getter private final GamePieceStats stats;
     @Getter private final GamePieceType type;
     @Getter private PieceAlignment alignment;
@@ -24,38 +24,12 @@ public abstract class GamePiece extends AbstractTexture implements Renderable, C
     @Getter private Renderable sprite;
     @Getter private final HashMap<GamePieceData, Object> data = new HashMap<>();
 
-    private OnClick onClick = null;
-    @Getter private ClickableEffectData clickableEffectData = null;
-
     public GamePiece(GamePieceStats stats, GamePieceType type, PieceAlignment alignment, UUID id, Renderable sprite) {
         this.stats = stats;
         this.type = type;
         this.alignment = alignment;
         this.id = id;
-        setSprite(sprite);
-
-        this.clickableEffectData = ClickableEffectData.getImmediate();
-        setClickableEffect(
-            (e) -> {
-                System.out.println("GAMEPIECE CLICKED");
-            },
-            clickableEffectData
-        );
-    }
-
-    public void setSprite(Renderable sprite) {
         this.sprite = sprite;
-        this.setBounds(sprite.getBounds());
-    }
-
-    public void setClickableEffect(OnClick onClick, ClickableEffectData effectData) {
-        this.onClick = onClick;
-        this.clickableEffectData = effectData;
-    }
-
-    public void triggerClickEffect(HashMap<Integer, CustomBox> interactionEntities) {
-        if (this.onClick == null) return;
-        this.onClick.run(interactionEntities);
     }
 
     public Object getData(GamePieceData key) {
@@ -64,20 +38,5 @@ public abstract class GamePiece extends AbstractTexture implements Renderable, C
 
     public void updateData(GamePieceData key, Object value) {
         data.put(key, value);
-    }
-
-    @Override
-    public List<Integer> getZs() {
-        return List.of(sprite.getZs().get(0));
-    }
-
-    @Override
-    public void render(SpriteBatch batch, int zLevel, boolean isPaused) {
-        sprite.render(batch, zLevel, isPaused);
-    }
-
-    @Override
-    public void render(SpriteBatch batch, int zLevel, boolean isPaused, int x, int y) {
-        sprite.render(batch, zLevel, isPaused, x, y);
     }
 }
